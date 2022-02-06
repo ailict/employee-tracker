55339aa5edd88e31f5948139d571231148d14919
ailict <ailichatfieldtaylor@gmail.com>
2022-02-05 19:40:59 -0500

    create select option function that allows users to select action to take in employee db

--- server.js
+++ server.js
@@ -18,3 +18,39 @@ const add_department = 'Add a department';
 const add_role = 'Add a role';
 const add_employee = 'Add an employee';
 const update_role = 'Update an employee role';
+
+var options = [view_departments, view_roles, view_employees, add_department, add_role, add_employee, update_role];
+
+function selectOption() {
+	return readlineSync.keyInSelect(options, "What would you like to do?");
+}
+
+function callback(answer) {
+	//console.info(answer.name);
+  	switch(answer) {
+  		case 0:
+      		viewDepartments();
+      		break;
+      	case 1:
+      		viewRoles();
+      		break;
+      	case 2:
+      		viewEmployees();
+      		break;
+      	case 3:
+      		addDepartment();
+      		break;
+      	case 4:
+      		addRole();
+      		break;
+      	case 5:
+      		addEmployee();
+      		break;
+      	case 6:
+      		updateRole();
+      		break;
+      	default:
+      		console.info("option entered is not available");
+      		break;
+  	}
+}
