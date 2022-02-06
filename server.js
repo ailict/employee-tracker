1fa95e621970d2b52381a521149ee8103260dd66
ailict <ailichatfieldtaylor@gmail.com>
2022-02-05 19:39:40 -0500

    created constants

--- server.js
+++ server.js
@@ -1,27 +1,20 @@
-const express = require('express');
-const db = require('./db/connection');
-const apiRoutes = require('./routes/apiRoutes');
+const mysql = require('mysql'); 
+const readlineSync = require('readline-sync');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+const db = mysql.createConnection({
+    host: 'localhost', 
+    port: '3306',
+    user: 'root',
+    password: 'root',
+    database: 'learningsql',
+    socketPath: '/tmp/mysql.sock'
+}); 
 
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
-// Use apiRoutes
-app.use('/api', apiRoutes);
-
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-});
-
-// Start server after DB connection
-db.connect(err => {
-  if (err) throw err;
-  console.log('Database connected.');
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-});
+const view_departments = 'View all departments';
+const view_roles = 'View all roles';
+const view_employees = 'View all employees';
+const add_department = 'Add a department';
+const add_role = 'Add a role';
+const add_employee = 'Add an employee';
+const update_role = 'Update an employee role';
